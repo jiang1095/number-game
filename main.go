e79b1915bb934ccf122aaa336c850ea713b99b14
姜培 <jiangpei@aobi.com>
2017-08-08 16:43:14 +0800

    添加猜数字的玩法

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"math"
 	"strconv"
-	"github.com/jiang1095/number-game/old"
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"regexp"
+	"time"
 )
 
 var number_set []string
@@ -22,6 +26,65 @@ func init() {
 				number_set = append(number_set, "0"+strconv.Itoa(i))
 			} else {
 				number_set = append(number_set, strconv.Itoa(i))
+			}
+		}
+	}
+}
+
+func main() {
+	var game_type int
+	fmt.Println("这是一个猜数字小游戏，你可以选择猜电脑生成的数字，或者让电脑猜你给出的数字：")
+	fmt.Println("\t1. 你来猜")
+	fmt.Println("\t2. 让我猜")
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		for {
+			fmt.Print("请选择游戏模式(1/2): ")
+			data, _, _ := reader.ReadLine()
+			game_type, _ = strconv.Atoi(string(data))
+			if game_type == 1 {
+				fmt.Println("你来猜我的数字")
+				numberGame()
+				break
+			} else if game_type == 2 {
+				fmt.Println("我来猜你的数字")
+
+				break
+			}
+		}
+		fmt.Print("再来一次？(y/n): ")
+		data, _, _ := reader.ReadLine()
+		switch string(data) {
+		case "y":
+		case "n":
+			os.Exit(0)
+		default:
+			os.Exit(1)
+		}
+	}
+}
+
+func numberGame() {
+	reader := bufio.NewReader(os.Stdin)
+	rand.Seed(time.Now().UnixNano())
+	number := number_set[rand.Intn(5040)]
+	for i := 0; i < 10; i++ {
+		fmt.Printf("请输入你的猜测(还剩%d次机会): ", 10-i)
+		data, _, _ := reader.ReadLine()
+		if match, _ := regexp.Match("^[0-9]*$", data); len(data) != 4 || !match {
+			fmt.Println("输入非法，请重新输入。")
+			i--
+			continue
+		}
+		guess := string(data)
+		a, b := compare(number, guess)
+		if a == 4 {
+			fmt.Printf("恭喜你在第%d次猜出了这个数字！\n", i+1)
+			break
+		} else {
+			fmt.Printf("本次猜测结果为: %dA%dB\n", a, b)
+			if i == 9 {
+				fmt.Printf("很抱歉，你没能在十次以内猜到数字。正确答案是: %s\n", number)
 			}
 		}
 	}
@@ -46,20 +109,6 @@ func compare(base, guess string) (int, int) {
 	return a, b
 }
 
-func main() {
-	//var set map[string][]string = make(map[string][]string)
-	//for _, v := range number_set {
-	//	a, b := compare("0123", v)
-	//	key := strconv.Itoa(a) + "A" + strconv.Itoa(b) + "B"
-	//	set[key] = append(set[key], v)
-	//}
-	//for k, v := range set {
-	//	fmt.Println(k, max_min(v))
-	//}
-	root := old.NewTree(number_set)
-	root.Print()
-}
-
 func max_min(num_set []string) string {
 	var result = ""
 	var score = math.MaxInt32
@@ -76,7 +125,6 @@ func max_min(num_set []string) string {
 				num_score = len(v)
 			}
 		}
-		fmt.Println(num_score)
 		if num_score < score {
 			score = num_score
 			result = base
